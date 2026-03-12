a799f1958e5f6c626ba83a2e38a3e818b2173751
tbk0809 <boonkaitan0809@gmail.com>
2026-03-12 20:04:23 +0800

    Add database connection setup with MySQL

---
+++ database.js
@@ -0,0 +1,24 @@
+
+const mysql = require('mysql2');//for the sql
+
+//the first one is must ,second one can be any other language wanna interact with
+
+
+const db=mysql.createConnection({//createConnection ,a tool to connect with mysql
+
+  host: 'localhost',
+  user: `root`,
+  password: `TBK@89boonkai`,
+  database: `tododb`
+});
+
+db.connect((err)=>{
+  if(err){
+    console.error("unable to connect");
+  }
+  else{
+    console.log("successfully connected");
+  }
+});
+
+module.exports=db;
