528f31eedd41a7c01e03987f693725d042c7cb3b
tbk0809 <boonkaitan0809@gmail.com>
2026-03-12 20:04:45 +0800

    Add CRUD routes for todos management
    Implement CRUD operations for todos in routes.js

---
+++ routes.js
@@ -0,0 +1,65 @@
+const db =require('./database.js');
+
+const express=require('express');//use the express again
+const router=express.Router();//create the router 
+
+router.get('/',(req,res)=>{//'/'for the visitor,req=request to visit, res =res later have 
+
+  const sql="SELECT * from todos";//sql code
+  db.query(sql,(err,results)=>{
+    if(err){
+      return res.status(500).send("Database error");//res.status(500)is a web code for error,send("database error")=human message
+    }
+      else{
+      res.json(results);//to access the result, like the actual way to see the message , we need to convert it to json files so we can see the results
+      }
+  });
+});
+router.post('/',(req,res)=>{
+
+  const title=req.body.title;//the text that user type at the frontend;
+  const sql="INSERT INTO todos (title) VALUES (?)";
+  db.query(sql,[title],(err,results)=>{
+    if(err){
+      return res.status(500).send("Database error");//res.status(500)is a web code for error,send("database error")=human message  
+    }
+    else{
+      res.json("tasks added succesfully");
+    }
+  });
+});
+router.delete('/:id',(req,res)=>{
+
+  const id=req.params.id;//the id that user wants to delete;
+   const sql="DELETE FROM todos WHERE id=?";
+  db.query(sql,[id],(err,results)=>{
+    if(err){
+      return res.status(500).send("Database error");//res.status(500)is a web code for error,send("database error")=human message  
+    }
+    else{
+      res.json("tasks deleted succesfully");
+    }
+  });
+});
+
+
+router.patch('/:id',(req,res)=>{
+   const id=req.params.id;//same as before
+   const sql="UPDATE todos SET completed = TRUE WHERE id=?";//the ?is a blank space/variable that handle the value/id from user 
+  db.query(sql,[id],(err,results)=>{
+    if(err){
+      return res.status(500).send("Database error");//res.status(500)is a web code for error,send("database error")=human message  
+    }
+    else{
+      res.json("tasks updated succesfully");
+    }
+  });
+});
+
+module.exports=router;
+
+
+
+
+
+
